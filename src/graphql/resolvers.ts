457e9dbca242b973724d22917b8c9e550159f45d
Arijit <arijitdasofficial@gmail.com>
2025-01-21 01:28:01 +0530

    feat: graphql

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -2,7 +2,14 @@ import { db } from '../db/db.js';
 
 export const resolvers = {
   Query: {
-    allUsers: () => {
+    getUser: (parent: unknown, args: { id: string }, context: unknown) => {
+      const id = args.id;
+      const user = db.users.find((e) => e.id === id);
+      if (!user) return null;
+      return user;
+    },
+
+    getAllUsers: () => {
       return db.users;
     },
   },
